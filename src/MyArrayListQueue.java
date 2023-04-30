c58452426dc59e9a7456b49809e4a26314789740
arunazturganbai <arunazturganbai@gmail.com>
2023-04-30 21:00:16 +0600

    .

---
+++ src/MyArrayListQueue.java
@@ -0,0 +1,11 @@
+import java.util.NoSuchElementException;
+import java.util.ArrayList;
+
+public class MyArrayListQueue<T> {
+    private final MyArrayList<T> queue;
+
+    public MyArrayListQueue() {
+        queue = new MyArrayList<>();
+    }
+
+

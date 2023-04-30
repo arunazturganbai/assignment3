25b16d68899a0dfb89f5a8e29de38955c83062d1
arunazturganbai <arunazturganbai@gmail.com>
2023-04-30 21:07:51 +0600

    .

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,23 +1,29 @@
 public class MyLinkedList {
+    //addFirst: which adds a new element at the beginning of the linked list
     public <E> void addFirst(E element) {
     }
 
+    //removeFirst: which removes and returns the first element from the linked list
     public <E> E removeFirst() {
         return null;
     }
 
+    //getFirst: which returns the first element from the linked list without removing it
     public <E> E getFirst(){
         return null;
     }
 
+    //isEmpty: which returns a boolean value indicating whether the linked list is empty or not
     public boolean isEmpty() {
         return false;
     }
 
+    //size: which returns an integer representing the number of elements in the linked list
     public int size() {
         return 0;
     }
 
+    //addLast: which adds a new element at the end of the linked list
     public <T> void addLast(T item) {
     }
 }

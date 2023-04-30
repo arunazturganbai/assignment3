0c31ed773ab0606ac1a575b9cd78755db8377a2d
arunazturganbai <arunazturganbai@gmail.com>
2023-04-30 20:51:16 +0600

    .

---
+++ src/MyArrayList.java
@@ -0,0 +1,20 @@
+public class MyArrayList<E> {
+    public void add(E element) {
+    }
+
+    public E remove(int i) {
+        return null;
+    }
+
+    public E get(int i) {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+}

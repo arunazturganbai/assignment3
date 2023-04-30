44bde3cb8dad3dc8b1b00046c592682c3d79f8f2
arunazturganbai <arunazturganbai@gmail.com>
2023-04-30 20:32:21 +0600

    created MyLinkedList class

---
+++ src/MyLinkedList.java
@@ -0,0 +1,20 @@
+public class MyLinkedList<E> {
+    public void addFirst(E element) {
+    }
+
+    public E removeFirst() {
+        return null;
+    }
+
+    public E getFirst() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+}

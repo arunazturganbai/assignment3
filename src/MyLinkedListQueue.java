7b9ffb92eae147627cec7973103e6c00a9434d0f
arunazturganbai <arunazturganbai@gmail.com>
2023-04-30 20:57:29 +0600

    .

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,9 +1,38 @@
+import java.util.NoSuchElementException;
+
 public class MyLinkedListQueue<T> {
     private final MyLinkedList queue;
 
 
-    public MyLinkedListQueue(MyLinkedList queue) {
-        this.queue = queue;
+    public MyLinkedListQueue() {
+        queue = new MyLinkedList();
     }
+
+    public void enqueue(T item) {
+        queue.addLast(item);
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return queue.removeFirst();
+    }
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return queue.getFirst();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+
 }
 
